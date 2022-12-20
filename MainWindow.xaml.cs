090818f4630eabb1592efc529dc971a469455d81
denis <106389581+fishman123456@users.noreply.github.com>
2022-12-20 11:49:24 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20_12_2022_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void _1_Красный_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _2_Оранжевый_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _3_Желтый_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _4_Зеленый_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _5_Синий_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void _6_Фиолетовый_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
